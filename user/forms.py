4078b74caf6c004710505f1c2cd85676bd13b060
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-06-26 17:14:24 +0900

    Django User Login 구현

---
+++ user/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import User
+from django.contrib.auth.forms import AuthenticationForm
+# Form: html에 있는 form 태그
+# Model Form: model을 사용하는 form
+# 유효성 검사를 위해서 이렇게 사용함
+class RegisterForm(forms.ModelForm):
+    
+    class Meta:
+        model = User
+        fields = ['email', 'name', 'password']
+
+
+class LoginForm(forms.ModelForm):
+    
+    class Meta:
+        model = User
+        fields = ['email', 'password']
