f3a936d7fad5282d8e63319c57508459779ad645
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-06-26 17:50:55 +0900

    Django Login 기능 확인

--- user/forms.py
+++ user/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from .models import User
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 # Form: html에 있는 form 태그
 # Model Form: model을 사용하는 form
 # 유효성 검사를 위해서 이렇게 사용함
--- user/models.py
+++ user/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    username = models.CharField(max_length=40)
     email = models.EmailField(unique=True, max_length=255)
     name = models.CharField(max_length=50, null=True, blank=True)
     password = models.CharField(max_length=50)
