233e3b62eab6f23c9db32b9f4903e3f69e1bbbda
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-06-27 18:51:10 +0900

    django autu test

--- user/forms.py
+++ user/forms.py
@@ -1,9 +1,7 @@
 from django import forms
 from .models import User
-from django.contrib.auth.forms import UserCreationForm
-# Form: html에 있는 form 태그
-# Model Form: model을 사용하는 form
-# 유효성 검사를 위해서 이렇게 사용함
+
+
 class RegisterForm(forms.ModelForm):
     
     class Meta:
@@ -16,3 +14,7 @@ class LoginForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['email', 'password']
+        widgets = {
+            'email': forms.EmailInput(attrs={'placeholder': 'email'}),
+            'password': forms.PasswordInput(attrs={'placeholder': 'password'}),
+        }
--- user/models.py
+++ user/models.py
@@ -3,13 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    username = models.CharField(max_length=40)
     email = models.EmailField(unique=True, max_length=255)
     name = models.CharField(max_length=50, null=True, blank=True)
     password = models.CharField(max_length=50)
-    registerd_date = models.DateField(auto_now_add=True)
+    registered_date = models.DateTimeField(auto_now_add=True)
     
-    # 아이디로 지정
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
     
