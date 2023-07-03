d2a5cb0e41aac5a920414f62d266b41022605502
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-07-03 14:31:14 +0900

    Django Create user error 수정

--- user/models.py
+++ user/models.py
@@ -1,40 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils import timezone
+
 # Create your models here.
 '''
-Auth User Model
+Auth User model
 - 생성
 - 삭제
 - 수정
 --> UserManager helper class 도움주는 클래스
-* 오버라이딩
-- 클래스 상속 후 수정
 '''
+
 class UserManager(BaseUserManager):
     
     def _create_user(self, email, password, is_staff, is_superuser, **extra_fields):
         if not email:
             raise ValueError('User must have an email')
-        now = timezone.now
+        now = timezone.now()
         email = self.normalize_email(email)
         user = self.model(
-            email = email,
-            is_staff = is_staff,
-            is_active = True,
-            is_superuser = is_superuser,
-            last_join = now,
-            date_joined = now,
+            email=email,
+            is_staff=is_staff,
+            is_active=True,
+            is_superuser=is_superuser,
+            last_login=now,
+            date_joined=now,
             **extra_fields
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
-    
     # create_user
     def create_user(self, email, password, **extra_fields):
         return self._create_user(email, password, False, False, **extra_fields)
-    
     # create_superuser
     def create_superuser(self, email, password, **extra_fields):
         return self._create_user(email, password, True, True, **extra_fields)
