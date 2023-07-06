6bbf15fcad41008f083712443a55596f80f83b8c
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-07-06 15:18:51 +0900

    RestApp create

---
+++ RestApp/blog/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    writer = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    
+class Comment(models.Model):
+    # on_delete는 참조하는 post가 지워질 경우 같이 삭제한다는 옵션
+    post = models.ForeignKey(Post, on_delete=models.CASCADE) 
+    content = models.TextField()
+    writer = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'Comment on {self.post.title}'
+
+
+class HashTag(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    writer = models.CharField(max_length=30)
+    name = models.CharField(max_length=10)
+    
+    def __str__(self):
+        return self.name
---
+++ RestApp/blog/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+# from . import views
+# from blog.views import Index, Write, Update, Delete, DetailView,CommentWrite,CommentDelete,HashTagWrite,HashTagDelete
+
+app_name = 'blog'
+
+urlpatterns = [
+    # # path(패턴, 매핑) /blog/
+    # # path("", views.index), # FBV
+    # # 글 목록 조회
+    # path("", Index.as_view(), name='list'), # /blog/
+    # # 글 상세 조회
+    # # path("detail/<int:pk>/", Detail.as_view(), name='detail'),
+    # path("detail/<int:pk>/", DetailView.as_view(), name='detail'),
+    # # 글 작성
+    # path("write/", Write.as_view(), name='write'), # /blog/write/
+    # # 글 수정
+    # path("detail/<int:pk>/edit", Update.as_view(), name='edit'),
+    # # 글 삭제
+    # path("detail/<int:pk>/delete", Delete.as_view(), name='delete'),
+    # # 코멘트 작성
+    # path("detail/<int:pk>/comment/write", CommentWrite.as_view(), name='cm-write'),
+    # # 코멘트 삭제
+    # path("detail/comment/<int:pk>/delete", CommentDelete.as_view(), name='cm-delete'),
+    # # 태그 작성
+    # path("detail/<int:pk>/hashtag/write", HashTagWrite.as_view(), name='tag-write'),
+    # # 태그 삭제
+    # path("detail/hashtag/<int:pk>/delete", HashTagDelete.as_view(), name='tag-delete'),
+]
